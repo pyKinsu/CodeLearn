a4162462d2e5c1ca64b36ad659ed7d5aea0e1dd2
Kinsu Rajput <pykinsu@outlook.com>
2025-12-09 20:30:11 +0530

    Delete app/test/page.tsx

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAptJ-qyieZT51UT0hcUdNo-n1Ww6OGXrg",
-  authDomain: "simpleloginsiggn.firebaseapp.com",
-  projectId: "simpleloginsiggn",
-  storageBucket: "simpleloginsiggn.firebasestorage.app",
-  messagingSenderId: "68557054848",
-  appId: "1:68557054848:web:e378d6570fa2b5c7e81765",
-  measurementId: "G-FLYJDFZ1HJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
