4259f5a56bdd1b6c95cb19a8080d50aa7955df5f
Kinsu Rajput <pykinsu@outlook.com>
2025-12-31 08:59:04 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,17 +5,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAptJ-qyieZT51UT0hcUdNo-n1Ww6OGXrg",
-  authDomain: "simpleloginsiggn.firebaseapp.com",
-  projectId: "simpleloginsiggn",
-  storageBucket: "simpleloginsiggn.firebasestorage.app",
-  messagingSenderId: "68557054848",
-  appId: "1:68557054848:web:e378d6570fa2b5c7e81765",
-  measurementId: "G-FLYJDFZ1HJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
-
-
 
 // Initialize Firebase only if it hasn't been initialized already
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
