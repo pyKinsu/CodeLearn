05a165dd3693ccad35aa923e6917fc4ed1d8e5f7
Kinsu Rajput <pykinsu@outlook.com>
2026-01-28 17:27:01 +0530

    Update page.tsx

--- app/(User)/pyq/page.tsx
+++ app/(User)/pyq/page.tsx
@@ -80,7 +80,7 @@ const subjects: Subject[] = [
       2022: '/pdf/pyq/bca-1-sem/principles-of-management-and-organization/bca-1-sem-principle-of-management-and-organization-303104-2022.pdf',
       2021: '/pdf/pyq/bca-1-sem/principles-of-management-and-organization/bca-1-sem-principle-of-management-and-organization-303104-2021.pdf',
       2020: null,
-      2019: '/pdf/pyq/bca-1-sem/principles-of-management-and-organization/bca-1-sem-principle-of-management-and-organization-303104-2019.pdf',
+      2019: '/pdf/pyq/bca-1-sem/principles-of-management-and-organization/bca-1-sem-principles-of-management-and-organization-303104-2019.pdf',
       2018: '/pdf/pyq/bca-1-sem/principles-of-management-and-organization/bca-1-sem-principle-of-management-and-organization-303104-2018.pdf',
       2017: '/pdf/pyq/bca-1-sem/principles-of-management-and-organization/bca-1-sem-principle-of-management-and-organization-303104-2017.pdf',
       2016: null,
